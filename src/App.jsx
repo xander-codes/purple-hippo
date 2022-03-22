d2e14e314a17ca58aaae0f73df704217af876b11
Alex Anghel <alexisky@gmail.com>
2022-03-23 00:04:49 +0200

    f

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Home from "./Home";
+import Login from "./Login";
 
 export default () => (
     <>
         <h1>Welcome to React Vite Micro App!</h1>
         <p>Hard to get more minimal than this React app.</p>
         <Home/>
+        <Login/>
     </>
 );
