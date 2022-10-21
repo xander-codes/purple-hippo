f856893b7b5ba83db26bd63f5e4606acc5b11005
Alex Anghel <alexisky@gmail.com>
2022-10-21 19:31:48 +0300

    add test1

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Home from "./Home";
 import Login from "./Login";
+import {Test1} from "./Test1";
 
 export default () => (
     <>
-        <h1>Welcome to React Vite Micro App!</h1>
-        <p>Hard to get more minimal than this React app.</p>
-        <Home/>
-        <Login/>
+        {/*<h1>Welcome to React Vite Micro App!</h1>*/}
+        {/*<p>Hard to get more minimal than this React app.</p>*/}
+        {/*<Home/>*/}
+        {/*<Login/>*/}
+        <Test1/>
     </>
 );
---
+++ src/Test1.jsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import "./test.css"
+
+export const Test1 = () => {
+    return (
+        <div>
+            <div>
+                <h4>Standardul occupational se aplica doar instructorului preparator formare</h4>
+                <p className="correct">Standardul se aplică tuturor categoriilor de formatori, respectiv: formatorului responsabil cu
+                    activităţile teoretice, formatorului responsabil cu activităţile practice de dezvoltare a
+                    competenţelor
+                    profesionale. indrumătorului de practica, maistrului de ucenicie, formatorului de formatori;</p>
+                <p>Standardul ocupational se aplica doar formatorului de formatori si maistrului instructor;</p>
+                <p>Standardul occupational se aplica doar instructorului preparator formare;</p>
+                <br/>
+            </div>
+            <div>
+                <h4>Dezavantajele asezarii tip “sala de clasa” sunt:</h4>
+                <p className="correct">Nu permite interactiunea directa cu o parte din cursanti, nu asigura o vizibilitate si o auditie buna pentru toti cursantii;</p>
+                <p>Nu permite organizarea de verificari si de teste de evaluare;</p>
+                <p>Nu-i permite formatorului sa intervina eficient in dinamica comportamentului de grup;</p>
+                <br/>
+            </div>
+            <div>
+                <h4>Caracteristicile participanţilor la formare se referă la:</h4>
+                <p>Trasaturile de personalitate ale cursantilor, implicarea lor, modul in care reactioneaza la metodele de predare folosite de formator;</p>
+                <p className="correct">Caracteristicile de vârstă, culturale, religioase, de educaţie, de experienţă anterioară, aptitudinile personale, cerinţele şi nevoile speciale;</p>
+                <p>Angajatorii acestora, grupul din care fac parte, regiunea sau tara din care provin, obiceiurile si cutumele pe care si le-au insusit;</p>
+                <br/>
+            </div>
+            <div>
+                <h4>Ce reprezinta metoda SMART?</h4>
+                <p>Este o tehnica de comunicare eficienta necesara comunicarii cu grupurile si cu persoanele;</p>
+                <p className="correct">Este o tehnica de scriere a obiectivelor;</p>
+                <p>Este o metoda de predare eficienta folosita in cadrul programelor de pregatire;</p>
+                <br/>
+            </div>
+            <div>
+                <h4>Care sunt elementele ce alcatuiesc continutul unei programe de pregatire?</h4>
+                <p className="correct">Obiectul de studiu, continutul tematic defalcat pe capitole/teme, perioadele de timp alocate, obiectivele de referinta, metode de realizare a instruirii, mijloace si materiale necesare sustinerii activitatilor, metode de evaluare;</p>
+                <p>Numarul curent, metodele de predare, obiectivele furnizorului de formare, metodele de scriere a obiectivelor, mijloacele tehnice necesare, metodele de implementare;</p>
+                <p>Titlul cursului, standardul occupational, codul COR, numarul capitolelor, numarul orelor de curs, locul sustinerii cursului, dotarile necesare sustinerii cursului, materialele de curs, metode de evaluare;</p>
+                <br/>
+            </div>
+            <div>
+                <h4>Studiul de caz poate fi definit ca:</h4>
+                <p className="correct">Tehnica prin care cursanţii sunt ajutaţi să dezvolte abilităţi analizând şi soIuţionând probleme specifice care pot aparea Ia locul de muncă sau in cadrul activitatii desfasurate, cazul fiind o situatie reală ce este prezentată grupului, care analizează aspectele problemei şi oferă una sau mai multe solutii;</p>
+                <p>Studiul de caz reprezinta o metoda prin care cursantii sunt chemati sa vina cu solutii la rezolvarea unei probleme si sa-si aduca aportul individual pentru aflarea celei mai bune cai de abordare a problemei respective;</p>
+                <p>Studiul de caz este un joc de rol in care unul din cursanti simuleaza o anumita stare sau imita un anumit personaj, cursantii analizand cea mai buna cale de raspuns la acea situatie si cea mai buna metoda de abordare;</p>
+                <br/>
+            </div>
+            <div>
+                <h4>Elementele comunicarii sunt:</h4>
+                <p>Elementele comunicarii sunt:</p>
+                <p className="correct">Comunicarea verbala, comunicarea nonverbala, comunicarea paraverbala;</p>
+                <p>Comunicarea cu ajutorul gesturilor, comunicarea cu ajutorul zambetului, comunicarea cu ajutorul inflexiunilor vocii.</p>
+                <br/>
+            </div>
+            <div>
+                <h4>Doar una din variantele de mai jos enumera corect o parte din metodele de evaluare folosite in formarea adultilor, va rugam sa precizati care:</h4>
+                <p>Identificarea nevoilor de invatare individuale, identificarea nevoilor de invatare de grup, identificarea nivelului general al grupei, stabilirea cantitatii de cunostinte insusite, follow-up;</p>
+                <p className="correct">Tema pentru acasa, proba practica, roleplay, observatia directa, chestionarul, focus grupuri;</p>
+                <p>Examenul, prezentarea personala, prezentarea grupului, evaluarea dinamicii de grup, feedback-ul, itemii de evaluare;</p>
+                <br/>
+            </div>
+            <div>
+                <h4>Care din urmatoarele definitii a Andragogiei este cea corecta?</h4>
+                <p>Andragogia este ştiinţa educarii formatorilor si formatorilor de formatori;</p>
+                <p>Andragogia este ştiinţa care are scopul de a dezvolta aptitudinile si abilitatile participantilor;</p>
+                <p className="correct">Andragogia este ştiinţa care aplica si dezvolta metodele teoretice si practice de educaţie a adulţilor;</p>
+                <br/>
+            </div>
+        </div>
+    );
+}
